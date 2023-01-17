b992c96010a17f439e22a48a7bf63258c98554f3
ayasalem123 <salem.aya184@gmail.com>
2023-01-17 12:11:03 +0100

    addin middlweare

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.set('view engine','ejs');
-var dates = require('express-dates-middleware');
+
+app.use((req,res,next)=>{
 const d = new Date();
 let day = d.getDay();
-let hours=d.getHours()
-if(0<=day && day<=5 && 8<=hours && hours<=16){
+let hours=d.getHours();
+if(0<=day && day<=5 && 8<=hours && hours<=16)
+{next();}
+else{
+       res.render('error.ejs')
+ }
+})
 app.get('/', function (req, res) {
    res.render('homePage.ejs');
  
@@ -19,9 +25,5 @@ app.get('/ourServices',function(rea,res){
     res.render('ourServices.ejs')
 }
 )
-}
-else{
-    app.use((req, res, next) => {
-        res.render('error.ejs')})
-}
+
 app.listen(3001)
